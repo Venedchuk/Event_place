cc7f82c84df6336f53d3491a0ef68e09e26e5f74
Venedchuk <sneyp67@gmail.com>
2016-11-28 16:09:21 +0200

    Not add place to event

---
+++ backend/models/Event.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "event".
+ *
+ * @property integer $id
+ * @property string $event_name
+ * @property string $date
+ *
+ * @property PlaceEvent[] $placeEvents
+ * @property Place[] $places
+ */
+class Event extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'event';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['date'], 'safe'],
+            [['event_name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'event_name' => 'Event Name',
+            'date' => 'Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPlaceEvents()
+    {
+        return $this->hasMany(PlaceEvent::className(), ['event_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPlaces()
+    {
+        return $this->hasMany(Place::className(), ['id' => 'place_id'])->viaTable('place_event', ['event_id' => 'id']);
+    }
+}
